596f99b40f68c4e7ee9e91369a07c527c392f6be
KenjiCrosland <kenji.crosland@gmail.com>
2014-06-28 13:05:08 -0700

    Initial Commit

---
+++ Homepwner/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Homepwner
+//
+//  Created by Kenji Crosland on 6/28/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
